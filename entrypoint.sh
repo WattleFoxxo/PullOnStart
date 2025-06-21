197bb55a8441fa5f55b6f9dbd5b65e12d6f0976f
WattleFoxxo <wattle@wattlefoxxo.au>
2025-06-21 17:13:43 +1000

    Init

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+
+echo "$GIT_KEY" > /root/.ssh/id_git_key
+chmod 600 /root/.ssh/id_git_key
+
+ssh-keyscan $GIT_PROVIDER >> /root/.ssh/known_hosts
+
+eval "$(ssh-agent -s)"
+ssh-add /root/.ssh/id_git_key
+
+if [ -d "/root/data/.git" ]; then
+    cd /root/data && git pull
+else
+    git clone "$GIT_REPO" /root/data
+fi
+
+if [ "$EXIT_WHEN_DONE" = "true" ]; then
+    exit 0
+else
+    tail -f /dev/null
+fi
